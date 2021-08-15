6b855891d5a4516dd2b0e35627b9245929218269
Pranay Prakash Dhavade <86852839+Dhavade@users.noreply.github.com>
2021-08-15 16:18:18 +0530

    Add files via upload

---
+++ linkedlist/single-linkedlist/traversal.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.ref=None
+
+class linkedlist:
+    def __init__(self):
+        self.head=None
+
+    def print_ll(self):
+        if self.head is None:
+            print("linked list is empty !")
+        else:
+            n=self.head
+            while n is not None:
+                print(n.data)
+                n=n.ref
+
+
+ll1=linkedlist()
+ll1.print_ll()                               
